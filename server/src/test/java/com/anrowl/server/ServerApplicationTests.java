248aa89fb1683845c75988b3300a7c8d2ebde016
androowl <multiad05@gmail.com>
2021-12-19 13:20:56 +0900

    Initial commit

---
+++ server/src/test/java/com/anrowl/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anrowl.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
